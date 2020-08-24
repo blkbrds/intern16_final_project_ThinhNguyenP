9f609b4e49188514b3df443f56b796dcc70e94aa
Van Le H <van.le@asiantech.vn>
2020-08-24 16:32:12 +0700

    Added project folder

---
+++ TheRestaurants/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  TheRestaurants
+//
+//  Created by Van Le H. on 8/24/20.
+//  Copyright © 2020 Thinh Nguyen P[6] All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
