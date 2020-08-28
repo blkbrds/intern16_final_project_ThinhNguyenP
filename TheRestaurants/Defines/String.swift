f4fc79c75cf4ce3e92b498cd82023f1841aa80dd
Van Le H <van.le@asiantech.vn>
2020-08-28 17:04:47 +0700

    Removed test code

--- TheRestaurants/Defines/String.swift
+++ TheRestaurants/Defines/String.swift
@@ -8,6 +8,4 @@
 
 import Foundation
 
-extension App.String {
-    static let test = "Click on either Details link, and you’ll be taken to Travis’ output. You can scroll through and see the details of how your project was built and how your tests ran, but the bottom line – and the good news – is all the way at the end:"
-}
+extension App.String { }
--- TheRestaurants/View/Home/HomeViewController.swift
+++ TheRestaurants/View/Home/HomeViewController.swift
@@ -12,7 +12,6 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var a:
         // Do any additional setup after loading the view.
     }
 }
