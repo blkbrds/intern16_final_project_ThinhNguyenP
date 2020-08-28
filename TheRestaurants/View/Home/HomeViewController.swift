dedaf09ac8032069800728521880d34d4c46ba6e
Van Le H <40164535+at-vanle@users.noreply.github.com>
2020-08-28 16:01:08 +0700

    Test error CI

--- TheRestaurants/View/Home/HomeViewController.swift
+++ TheRestaurants/View/Home/HomeViewController.swift
@@ -12,7 +12,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var a:
         // Do any additional setup after loading the view.
     }
 }
