870448149863ab7bd1167c75dc7297a35d69266a
thinhnguyen6 <thinh.nguyen6@asiantech.vn>
2020-09-14 17:36:13 +0700

    create ui detail overview

---
+++ TheRestaurants/View/Detail/OverViewDetail/Header/HeaderDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderDetailView.swift
+//  TheRestaurants
+//
+//  Created by PCI0019 on 9/14/20.
+//  Copyright © 2020 Thinh Nguyen P[6]. All rights reserved.
+//
+
+import UIKit
+
+class HeaderDetailView: UIView {
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var nameRestaurantLabel: UILabel!
+    @IBOutlet weak var cuisineLabel: UILabel!
+    
+    func configView() {
+        
+    }
+}
