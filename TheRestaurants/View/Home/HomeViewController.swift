cdf1ac2beb7b7748ee5d59cc8d0073702007cf8c
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-09-19 17:54:16 +0700

    fix navigation didn't is hidden

--- TheRestaurants/View/Home/HomeViewController.swift
+++ TheRestaurants/View/Home/HomeViewController.swift
@@ -110,7 +110,6 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
         viewController.viewModel = viewModel.didSelectRowAt(indexPath: indexPath)
         viewController.hidesBottomBarWhenPushed = true
         navigationController?.pushViewController(viewController, animated: true)
-        navigationController?.isNavigationBarHidden = true
     }
 }
 extension HomeViewController: UIScrollViewDelegate {
