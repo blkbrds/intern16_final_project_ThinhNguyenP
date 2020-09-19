d46c1a3f6279e9268347891f17971978c6e14257
Van Le H <van.le@asiantech.vn>
2020-09-19 12:36:38 +0700

    Added icons and rearranged

---
+++ TheRestaurants/View/Detail/OverViewDetail/OtherInfor/HighlightView/HightlightView.swift
@@ -0,0 +1,21 @@
+//
+//  HightlightView.swift
+//  TheRestaurants
+//
+//  Created by Van Le H. on 9/18/20.
+//  Copyright © 2020 Thinh Nguyen P[6] All rights reserved.
+//
+
+import UIKit
+
+class HightlightView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TheRestaurants/View/Detail/OverViewDetail/OtherInfor/OtherInfomationViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  OtherInfomationViewModel.swift
+//  TheRestaurants
+//
+//  Created by Van Le H. on 9/18/20.
+//  Copyright © 2020 Thinh Nguyen P[6] All rights reserved.
+//
+
+import Foundation
