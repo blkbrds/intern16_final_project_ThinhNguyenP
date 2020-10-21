0a0ff5787200ce93e4a338542ea968b096793eff
Thinh Nguyen P .[6] <thinh.nguyen6@asiantech.vn>
2020-10-21 14:43:38 +0700

    delete file not working

--- TheRestaurants/ReviewFinal/Bai2/DetailVM.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DetailVM.swift
-//  TheRestaurants
-//
-//  Created by ADMIN on 10/21/2563 BE.
-//  Copyright © 2563 BE Thinh Nguyen P[6]. All rights reserved.
-//
-
-import Foundation
--- TheRestaurants/ReviewFinal/Bai2/HomeVCModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HomeVCModel.swift
-//  TheRestaurants
-//
-//  Created by ADMIN on 10/21/2563 BE.
-//  Copyright © 2563 BE Thinh Nguyen P[6]. All rights reserved.
-//
-
-import Foundation
